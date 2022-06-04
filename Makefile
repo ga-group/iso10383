f7f342316afe42a9c9a030c1bf167b3e769be161
Sebastian Freundt <freundt@ga-group.nl>
2022-06-04 09:21:09 +0000

    provide normaliser code and routines to snarf straight from the ISO publication

--- Makefile
+++ Makefile
@@ -11,6 +11,25 @@ download/ISO10383_MIC_latest.xlsx:
 	| tr '@' '\001' \
 	| sort -u \
 	| tr '\001' '@' \
-	| ttl2ttl \
+	| ttl2ttl -B \
 	> $@ && mv $@ $<
 
+MarketsIndividuals.ttl: download/ISO10383_MIC_latest.xlsx MarketsIndividuals.ttl.aux
+	ttl2ttl --sortable $@.aux \
+	> $@.t
+	## make a black list
+	cut -f1 $@.t \
+	| sort | uniq \
+	> $@.tt
+	## we don't want to use lcc-3166-1 prefix
+	echo "lcc-3166-1:" >> $@.tt
+	-ttl2ttl --sortable $@ \
+	| grep -vFf $@.tt \
+	>> $@.t
+	$(RM) $@.tt
+	scripts/rdISO10383.R $< \
+	| tarql -t --stdin sql/ISO10383.tarql \
+	| sed 's@rdf:type@a@; s@  *@ @g' \
+	| tr -d '\200-\377' \
+	>> $@.t && mv $@.t $@
+	$(MAKE) $@.canon
--- scripts/rdISO10383.R
+++ scripts/rdISO10383.R
@@ -3,14 +3,69 @@
 library(readxl)
 library(data.table)
 
+capwords <- function(s, strict = FALSE)
+{
+	cap <- function(s) {
+		paste(toupper(substring(s, 1, 1)),tolower(substring(s, 2)), sep="", collapse=" ")
+	}
+	sapply(strsplit(s, split = " "), cap, USE.NAMES=!is.null(names(s)))
+}
+
+cities <- fread("iso	fibo
+Berne	Bern
+Boca Raton	BocaRaton
+Bryanston, Sandton	BryanstonSandton
+Bucharest	Bucarest
+Cluj Napoca	ClujNapoca
+Dar Es Salaam	Dar_es_Salaam
+Duesseldorf	Dusseldorf
+Cybercity, Ebene	Ebene
+Ebene City	Ebene
+Frankfurt Am Main	Frankfurt
+Gift City, Gandhinagar	GIFT_City
+Kyiv	Kiev
+Klagenfurt Am Woerthersee	Klagenfurt_am_Woerthersee
+Kuwait	Kuwait_City
+Lisboa	Lisbon
+Mexico	Mexico_City
+Milano	Milan
+Muenchen	Munich
+Delhi	New_Delhi
+Nicosia (lefkosia)	Nicosia
+Palma De Mallorca	PalmaDeMallorca
+Panama	Panama_City
+Vila	Port_Vila
+Port Of Spain	Port_of_Spain
+Rio De Janeiro	Rio_de_Janeiro
+Roma	Rome
+Saint-petersburg	Saint-Petersburg
+St Albans	StAlbans
+St.  Peter Port	Saint_Peter_Port
+Sea Girt	SeaGirt
+St Albans	StAlbans
+The Woodlands	TheWoodlands
+Torino	Turin
+Washington/new York	Washington_New_York
+S-hertogenbosch	s-Hertogenbosch
+")
+
 if (sys.nframe() == 0L) {
 	args <- commandArgs(trailingOnly=TRUE)
 	outf <- ""
 
-	x <- rbindlist(lapply(lapply(args, read_excel), as.data.table), use.names=TRUE)
+	x <- rbindlist(lapply(lapply(args, read_excel, na=c("n/a","N/A")), as.data.table), use.names=TRUE)
 	x[, WEBSITE:=tolower(WEBSITE)]
 	x[!is.na(WEBSITE) & !grepl("^https?://", WEBSITE), WEBSITE:=paste0("http://",WEBSITE)]
-	x[!is.na(WEBSITE) & !grepl("https?://.*/", WEBSITE), WEBSITE:=paste0(WEBSITE, "/")]
+#	x[!is.na(WEBSITE) & !grepl("https?://.*/", WEBSITE), WEBSITE:=paste0(WEBSITE, "/")]
+	## massage city names
+	x[!is.na(CITY), CITY:=capwords(CITY)]
+	x[cities, on=c(CITY="iso"), CITY:=i.fibo]
+
+	dtify <- function(fld) set(x, j=fld, value=as.IDate(x[[fld]], format="%Y%m%d"))
+	dtify("CREATION-ISO DATE")
+	dtify("MODIF-ISO DATE")
+	dtify("LAST VALIDATION MONTH")
+	dtify("EXPIRY DATE")
 
 	fwrite(x, outf, na="", sep='\t', quote=FALSE)
 }
